320f1c74d3f1e3cff88f244aa2fd69333cb4fbc0
Philipe Couto <philipephwd@gmail.com>
2018-06-13 15:01:42 -0300

    ....

--- php/config.php
+++ php/config.php
@@ -3,7 +3,9 @@ function Conectar(){
     try{
         $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
         $con = new PDO("mysql:host=mysql472.umbler.com; dbname=db_condominio;", "sup", "tecsis123", $opcoes);
+        echo "conectado com sucesso";
         return $con;
+
     } catch (Exception $e){
         echo 'Erro: '.$e->getMessage();
         return null;
