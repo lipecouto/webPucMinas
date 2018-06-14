8bdb11774154108808cb13fef3e802a0218392dd
Philipe Couto <philipephwd@gmail.com>
2018-06-14 11:14:08 -0300

    .

--- js/acoes.js
+++ js/acoes.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
       //Agora carrega os dados do apartamento selecionado
     $("#getCondominio").on("change", function(event){
-        var id_cond = $(this).val();
+        var id_cond = $("$getCondominio").val();
         alert(id_cond);
         $.getJSON('/php/service.php?acao=consultaAp&idcondomio='+id_cond, function (data){
             preencheSelectAp(data);
