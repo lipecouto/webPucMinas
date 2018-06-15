1ee47f7125b078b676f63e6696ade346f86d57c0
Philipe Couto <philipephwd@gmail.com>
2018-06-15 13:53:31 -0300

    .

--- php/service.php
+++ php/service.php
@@ -38,7 +38,7 @@
 
 			case 'listUser':
 				 $cond_2 = (isset($_GET['id_cond2'])) ? $_GET['id_cond2']: '';
-				 echo(listaUsuarios($condominio));
+				 echo(listaUsuarios($cond_2));
 				break;	
 
 			case 'login':
